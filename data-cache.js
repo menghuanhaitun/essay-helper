15958a52536a1626576da875737b42fdcb3f53ee
menghuanhaitun <ming.li8245@gmail.com>
2026-04-05 18:00:30 +0800

    chore: 更新内置数据缓存 (2026/4/5 18:00:30)

---
+++ data-cache.js
@@ -0,0 +1,20 @@
+/**
+ * 作文助手 - 内置数据缓存
+ * 
+ * 此文件由 export-data.js 自动生成，请勿手动编辑。
+ * 生成时间: 2026-04-05T10:00:30.627Z
+ * 
+ * 如需更新数据，运行: node export-data.js
+ */
+var BUILTIN_DATA = {
+  "vocab": {},
+  "upgradeExamples": [],
+  "upgradeRules": [],
+  "guideTemplates": {},
+  "allEssays": [],
+  "allPracticeTopics": [],
+  "_meta": {
+    "exportedAt": "2026-04-05T10:00:30.627Z",
+    "version": "1.0"
+  }
+};

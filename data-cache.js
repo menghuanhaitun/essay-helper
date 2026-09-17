a2adb0726c47d6476227886e139f1f7e4383a990
menghuanhaitun <ming.li8245@gmail.com>
2026-09-18 02:01:35 +0800

    chore: 自动更新内置数据缓存 (2026-09-17)

--- data-cache.js
+++ data-cache.js
@@ -1,11 +1,11 @@
 // 内置数据缓存 - 自动生成，请勿手动修改
-// 导出时间: 2026-09-16T18:00:57.936Z
+// 导出时间: 2026-09-17T18:01:34.007Z
 // 数据来源: Cloudflare D1 (essay-helper-db)
 // 自动更新: Cloudflare Workers Cron Trigger (每天凌晨2点)
 
 var BUILTIN_DATA = {
   "_meta": {
-    "exportedAt": "2026-09-16T18:00:57.936Z",
+    "exportedAt": "2026-09-17T18:01:34.007Z",
     "version": "1.0"
   },
   "vocab": {
